f14bc246842b6bf65d4fc00412fb2c123469b980
Waffen Sultan <waffen.sultan@gmail.com>
2025-05-18 14:29:27 +0800

    feat: added org profile and other changes

--- src/components/ui/avatar-card.tsx
+++ src/components/ui/avatar-card.tsx
@@ -2,10 +2,11 @@ import Typography from "@/components/typography/typography";
 
 interface AvatarCardProps {
     title: string;
+    secondaryTitle?: string;
     imageUrl: string;
 }
 
-const AvatarCard = ({ title, imageUrl }: AvatarCardProps) => {
+const AvatarCard = ({ title, secondaryTitle, imageUrl }: AvatarCardProps) => {
     return (
         <article className="relative flex aspect-square w-40 sm:w-52 md:w-64 lg:w-72 xl:w-80 rounded-2xl shadow-xl border border-muted/10">
             <img
@@ -16,6 +17,9 @@ const AvatarCard = ({ title, imageUrl }: AvatarCardProps) => {
             <section className="min-h-12 lg:min-h-18 w-full bg-primary/95 text-white py-1.5 px-3 rounded-b-2xl tracking-wide self-end z-50">
                 <Typography variant="h5" className="font-bold">
                     {title}
+                </Typography>
+                <Typography variant="h5" className="text-muted">
+                    {secondaryTitle}
                 </Typography>
             </section>
         </article>
