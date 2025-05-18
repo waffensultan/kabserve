1cb7d21c685d3965eebae17108e89188ccc539b3
Waffen Sultan <waffen.sultan@gmail.com>
2025-05-18 15:36:34 +0800

    feat: added profile overlay

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import Typography from "@/components/typography/typography";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -59,15 +67,41 @@ export default function Header() {
                     <Bell />
                 </li>
                 <li>
-                    <Link href={"/profile"}>
-                        <div className="w-10 h-10 p-0.5 rounded-full bg-[#27391C]">
-                            <img
-                                className="rounded-full"
-                                src="images/placeholder2.png"
-                                alt="placeholder"
-                            />
-                        </div>
-                    </Link>
+                    <DropdownMenu modal={false}>
+                        <DropdownMenuTrigger>
+                            <div className="w-10 h-10 p-0.5 rounded-full bg-[#27391C]">
+                                <img
+                                    className="rounded-full"
+                                    src="images/placeholder2.png"
+                                    alt="placeholder"
+                                />
+                            </div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="bg-primary/90 border-[#FFFDF6] backdrop-blur-md text-white">
+                            <DropdownMenuLabel className="flex flex-row items-center gap-2">
+                                <div className="w-10 h-10 p-0.5 rounded-full bg-[#27391C]">
+                                    <img
+                                        className="rounded-full"
+                                        src="images/placeholder2.png"
+                                        alt="placeholder"
+                                    />
+                                </div>
+                                <div className="flex flex-col">
+                                    <span>First Name Last Name</span>
+                                    <span className="font-normal text-xs">
+                                        firstnamelastname@cvsu.edu.ph
+                                    </span>
+                                </div>
+                            </DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem asChild className="font-semibold">
+                                <Link href={"/profile"}>View Profile</Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className="font-semibold">
+                                Sign Out
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </li>
             </ul>
         </nav>
